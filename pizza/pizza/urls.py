545d621e898afeb42294d9e877046e72bd53927c
Joe Yates <joe.g.yates@gmail.com>
2012-12-15 18:46:00 +0100

    Retrieve and view data

--- pizza/pizza/urls.py
+++ pizza/pizza/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^$', 'pizza.views.home', name='home'),
+    url(r'^$',       'pizza.views.home',   name='home'),
+    url(r'^pizzas$', 'pizza.views.pizzas', name='pizzas'),
 )
 
--- pizza/pizza/views.py
+++ pizza/pizza/views.py
@@ -1,6 +1,16 @@
 from django import http
+from django.shortcuts import render
+
+from pizza.models import *
 
 def home(request):
     html = "<h1>Hello World!</h1>"
     return http.HttpResponse(html)
+
+def pizzas(request):
+    pizzas = Pizza.objects.all()
+    context = {
+        'pizzas': pizzas
+    }
+    return render(request, 'pizza/pizzas.html', context)
 
